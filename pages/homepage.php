8519d7a62fce52a2f993295bdc8ca394968c8acf
Ali Can <doganalican@hotmail.com>
2022-10-19 21:11:58 +0300

    education and certificates section

--- pages/homepage.php
+++ pages/homepage.php
@@ -276,33 +276,90 @@
         background-color: var(--yellow);
     }
 
-    .about .skills .progress .bar:nth-child(1) h3{
+    .about .skills .progress .bar:nth-child(1) h3 {
         width: 95%;
     }
-    
-    .about .skills .progress .bar:nth-child(2) h3{
+
+    .about .skills .progress .bar:nth-child(2) h3 {
         width: 70%;
     }
-    
-    .about .skills .progress .bar:nth-child(3) h3{
+
+    .about .skills .progress .bar:nth-child(3) h3 {
         width: 90%;
     }
-    
-    .about .skills .progress .bar:nth-child(4) h3{
+
+    .about .skills .progress .bar:nth-child(4) h3 {
         width: 60%;
     }
-    
-    .about .skills .progress .bar:nth-child(5) h3{
+
+    .about .skills .progress .bar:nth-child(5) h3 {
         width: 80%;
     }
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+    .about .edu-exp .row {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1.5rem;
+    }
+
+    .about .edu-exp .row .box-container {
+        flex: 1 1 40rem;
+    }
+
+    .about .edu-exp .row .box-container .title {
+        padding: 1.5rem;
+        font-size: 3rem;
+        background-color: var(--yellow);
+    }
+
+    .about .edu-exp .row .box-container .box {
+        text-align: left;
+        margin: 1.5rem 0;
+        background-color: var(--light-bg);
+        padding: 2rem;
+    }
+
+    .about .edu-exp .row .box-container .box span {
+        font-size: 1.5rem;
+    }
+
+    .about .edu-exp .row .box-container .box h3 {
+        font-size: 2.5rem;
+        font-weight: normal;
+        margin-top: 1.rem;
+        margin-bottom: 1rem;
+    }
+
+    .about .edu-exp .row .box-container .box p {
+        line-height: 1.5;
+        font-size: 1.7rem;
+        line-height: 2;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     @media (max-width:991px) {
         html {
             font-size: 55%;
@@ -446,9 +503,108 @@
             </div>
         </div>
 
-    </section>
 
+
+        <div class="edu-exp">
+
+            <h1 class="heading"><span>Education & Experience</span></h1>
+
+            <div class="row">
+                <div class="box-container">
+                    <h3 class="title">Education</h3>
+                    <div class="box">
+                        <span>( 2019 - PRESENT )</span>
+                        <h3>Software Engineering</h3>
+                        <p>2019-PRESENT Engineering Department – Software Engineering Full Scholarship (%100 English)
+                            (Senior Class) GPA: 3.06/4.00 EUROPEAN UNIVERSITY OF LEFKE LEFKE, NORTHERN CYPRUS </p>
+                    </div>
+                    <div class="box">
+                        <span>( 2018 - 2019 )</span>
+                        <h3>English Preparatory School</h3>
+                        <p>2018-2019 English Preparatory School - Avarage: B2-Intermediate
+                            EUROPEAN UNIVERSITY OF LEFKE-PREP SCHOOL LEFKE, NORTHERN CYPRUS </p>
+                    </div>
+                    <div class="box">
+                        <span>( 2014 - 2018 )</span>
+                        <h3>Merkez Atatürk Anadolu High School</h3>
+                        <p>2014-2018 High School - Numerical - Avarage: 84/100
+                            Merkez Atatürk Anadolu High School KAHRAMANMARAS, TURKEY </p>
+                    </div>
+                </div>
+
+                <div class="box-container">
+                    <h3 class="title">Experience</h3>
+
+                    <div class="box">
+                        <span>( July 2022 - October 2022 )</span>
+                        <h3>Trainee at Kahramanmaras Teknokent</h3>
+                        <p>
+                            Project-Bus Tracking System <br>
+                            Role: Creating a website's interfaces with HTML and CSS, also some backend processes with python and flask.
+                            Technologies: Python, Flask, PHPMyAdmin, XAMPP, JavaScript, HTML, CSS, Visual Studio Code
+                        </p>
+                    </div>
+                </div>
+
+
+            </div>
+            <h1 class="heading"><span>Seminars & Certificates</span></h1>
+
+            <div class="row">
+                <div class="box-container">
+                    <h3 class="title">Seminars & Certificates</h3>
+
+                    <div class="box">
+                        <span>( 2019 )</span>
+                        <h3>Leadership and Creativity </h3>
+                        <p>
+                            Ada Eğitim Akademi
+                            Prof. Dr. İlber ORTAYLI, Prof. Dr. Uğur Batı, Uzm. Psk. Arif İlker Şahin – KKTC
+                        </p>
+                    </div>
+                    <div class="box">
+                        <span>( 2019 )</span>
+                        <h3>Anatomy of Creativity </h3>
+                        <p>
+                            Ada Eğitim Akademi
+                            Prof. Dr. İlber ORTAYLI, Prof. Dr. Uğur Batı, Uzm. Psk. Arif İlker Şahin – KKTC
+                        </p>
+                    </div>
+                    <div class="box">
+                        <span>( 2019 )</span>
+                        <h3>To be the leader of your own life</h3>
+                        <p>
+                            Ada Eğitim Akademi
+                            Prof. Dr. İlber ORTAYLI, Prof. Dr. Uğur Batı, Uzm. Psk. Arif İlker Şahin – KKTC
+                        </p>
+                    </div>
+                    <div class="box">
+                        <span>( 2019 )</span>
+                        <h3>ENGLISH PREPARATORY PROGRAMME</h3>
+                        <p>
+                            PEARSON ASSURED ORGANISATION / EUROPEAN UNIVERSITY OF LEFKE
+                        </p>
+                    </div>
+                    <div class="box">
+                        <span>( 2015 )</span>
+                        <h3>Intro to Computer Science Course </h3>
+                        <p>
+                            K-8 Intro to Computer Science Course
+                            Code.org | Hadi Partovi - AB
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+    </section>
     <!-- about section ends -->
+
+
+
+
+
+
+
 
 
 
